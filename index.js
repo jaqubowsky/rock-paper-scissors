5d9c1089a4d73617e516c8c21fd84e1ddd7093ad
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2022-12-25 21:53:38 +0100

    Add end of the game and icons appearing on choose

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const playerChoicePaper = document.querySelector(".paper-el");
 const playerChoiceScissors = document.querySelector(".scissors-el");
 const playerScore = document.getElementById("score-player-el");
 const computerScore = document.getElementById("score-computer-el");
+const scoreTextEl = document.querySelector(".score-text-el");
 const scoreSubtextEl = document.querySelector(".score-subtext-el");
+const playerSignIcon = document.querySelector(".playerIcon");
+const computerSignIcon = document.querySelector(".computerIcon");
 
 function getComputerChoice(arr) {
 	return arr[Math.floor(Math.random() * arr.length)];
@@ -16,12 +19,15 @@ function getComputerChoice(arr) {
 function updateMessage(playerSelection, computerSelection) {
 	if (isWinning(playerSelection, computerSelection) === true) {
 		playerScoreValue += 1;
-		scoreSubtextEl.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+		scoreSubtextEl.textContent = `${playerSelection} beats ${computerSelection}`;
+		scoreTextEl.textContent = "You win!";
 	} else if (isWinning(playerSelection, computerSelection) === false) {
 		computerScoreValue += 1;
-		scoreSubtextEl.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+		scoreSubtextEl.textContent = `${computerSelection} beats ${playerSelection}`;
+		scoreTextEl.textContent = "You lose!";
 	} else if (isWinning(playerSelection, computerSelection) === "tie") {
-		scoreSubtextEl.textContent = "It's a tie!";
+		scoreSubtextEl.textContent = `${playerSelection} ties with ${playerSelection}`;
+		scoreTextEl.textContent = "It's a tie!";
 	}
 	playerScore.textContent = playerScoreValue;
 	computerScore.textContent = computerScoreValue;
@@ -45,18 +51,49 @@ function isWinning(playerSelection, computerSelection) {
 	}
 }
 
-function game(playerSelection) {
-	updateMessage(playerSelection, getComputerChoice(choices));
+function updateScoreboard(playerSelection, computerSelection) {
+	switch (playerSelection) {
+		case "Rock":
+			playerSignIcon.textContent = "✊";
+			break;
+		case "Paper":
+			playerSignIcon.textContent = "✋";
+			break;
+		case "Scissors":
+			playerSignIcon.textContent = "✌";
+			break;
+	}
+
+	switch (computerSelection) {
+		case "Rock":
+			computerSignIcon.textContent = "✊";
+			break;
+		case "Paper":
+			computerSignIcon.textContent = "✋";
+			break;
+		case "Scissors":
+			computerSignIcon.textContent = "✌";
+			break;
+	}
+}
+
+function game(playerSelection, computerChoice) {
+	if (playerScoreValue >= 5 || computerScoreValue >= 5) {
+		return;
+	} else {
+		updateMessage(playerSelection, computerChoice);
+		updateScoreboard(playerSelection, computerChoice);
+	}
 }
 
 playerChoiceRock.addEventListener("click", function () {
-	game("Rock");
+	game("Rock", getComputerChoice(choices));
 });
 
 playerChoiceScissors.addEventListener("click", function () {
-	game("Scissors");
+	game("Scissors", getComputerChoice(choices));
 });
 
 playerChoicePaper.addEventListener("click", function () {
-	game("Paper");
+	game("Paper", getComputerChoice(choices));
 });
